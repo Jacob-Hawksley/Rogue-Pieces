08bdb31e4ecc13060ed660adb44a3aeb80aac331
Jacob-Hawksley <jacobmhawksley@gmail.com>
2025-10-17 17:33:42 +0100

    Added check for the white king for rooks,bishops and queens

--- king_w_area.gd
+++ king_w_area.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 var tile 
 var piece = 'wking'
+var check
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -122,3 +123,28 @@ func checkforspaces(space):
 	newletter = ord(space[0])
 	newnumber = int(space[1])
 	return(Main.spaces)
+
+
+func checkforcheck(space):
+	check = false
+	for i in range(-8,8):
+		for n in range(-8,8):
+			if Main.currentboard[str(char(space[0] + i))+space[1]] == 'brook':
+				check = true
+			elif Main.currentboard[str(char(space[0] + i))+space[1]] == null:
+				pass
+			else:
+				continue
+			if Main.currentboard[space[0]+str(int(space[1] + n))] == 'bbishop':
+				check = true
+			elif Main.currentboard[space[0]+str(char(space[0] + i))] == null:
+				pass
+			else:
+				continue
+			if Main.currentboard[str(char(space[0] + i))+str(int(space[1] + n))] == 'bqueen':
+				check = true
+			elif Main.currentboard[str(char(space[0] + i))+str(int(space[1] + n))] == null:
+				pass
+			else:
+				continue
+	
